8c770e0368f9e9ebe5ab36fc457d18067eff6d5b
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 11:20:29 -0300

    Doing Casting to convert between numeric types and other numerics types

---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salary;
+            salary = 1200.50;
+
+            int integerSalary; //32 bits
+            integerSalary = (int)salary;
+
+            Console.WriteLine(integerSalary);
+
+            long age; //64 bits
+            age = 13000000000;
+
+            Console.WriteLine(age);
+
+            short productsQuantity; // 16 bits
+            productsQuantity = 15000;
+
+            Console.WriteLine(productsQuantity);
+
+            float height = 1.80f;
+
+            Console.WriteLine(height);
+
+            Console.ReadLine();
+        }
+    }
+}
