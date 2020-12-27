2a1e40138ecd77d016fbeaaa43b29baaa837df62
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 11:52:34 -0300

    Attruting a variatle value to another

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int age = 32;
+            int gustavAge = age;
+
+            age = 20;
+
+            Console.WriteLine(age);
+            Console.WriteLine(gustavAge);
+
+            Console.ReadLine();
+        }
+    }
+}
