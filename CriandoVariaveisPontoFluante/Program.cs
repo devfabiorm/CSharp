92526b28eb1ada156f783b3cd6a30fc68b24baaf
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 11:06:10 -0300

    Creating variables double

---
+++ CriandoVariaveisPontoFluante/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoVariaveisPontoFluante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variáveis ponto fluante");
+
+            double salary;
+            salary = 1200.70;
+
+            Console.WriteLine(salary);
+
+            double age;
+            age = 15.0 / 2;
+
+            Console.WriteLine(age);
+
+            age = 5 / 3;
+
+            Console.WriteLine("5 / 3 = " + age);
+
+            age = 5.0 / 3;
+
+            Console.WriteLine("5.0 / 3 = " + age);
+
+            Console.WriteLine("A execução terminou. Tecle ENTER para finalizar...");
+            Console.ReadLine();
+        }
+    }
+}
