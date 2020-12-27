6ae66c3cd29778e0ad9904d6cc1bc858eb04c7fb
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 10:52:49 -0300

    Creating integer variables

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+            int age;
+            age = 32;
+
+            Console.WriteLine(age);
+
+            age = 10;
+
+            Console.WriteLine(age);
+
+            age = 10 + 5;
+
+            Console.WriteLine(age);
+
+            age = 10 + 5 * 2;
+
+            Console.WriteLine(age);
+
+            age = (10 + 5) * 2;
+
+            Console.WriteLine("Sua idade é " + age + "!");
+
+            Console.WriteLine("Execução finalizada. Tecle ENTER para sair");
+            Console.ReadLine();
+        }
+    }
+}
