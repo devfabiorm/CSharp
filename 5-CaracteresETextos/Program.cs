22b66b7f6fef429848c56637f890659577862d07
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 11:43:01 -0300

    Creating char and string variables

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char firtChar = 'a';
+            Console.WriteLine(firtChar);
+
+            firtChar = (char)65;
+            Console.WriteLine(firtChar);
+
+            firtChar = (char)(firtChar + 1);
+            Console.WriteLine(firtChar);
+
+            string title = "Alura Cursos de Tecnologia " + 2020;
+            string programmingCourses = @"- .NET
+- Java
+- JavaScript";
+
+            Console.WriteLine(title);
+            Console.WriteLine(programmingCourses);
+
+            Console.ReadLine();
+        }
+    }
+}
